434c6c24177aa1c9d86776de892670b1d623367f
Donghwi Park <donghwipark@gmail.com>
2020-10-16 22:51:05 +0900

    udemy

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
   onFormSubmit = event => {
     event.preventDefault();
 
-    this.props.onSubmit(this.state.term);
+    this.props.onSucbmit(this.state.term);
   }
 
   render() {
